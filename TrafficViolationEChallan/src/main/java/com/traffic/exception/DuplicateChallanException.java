7a0f84bbf1fcae92996e00fbd3845be3e86ef323
archanajawahar <archanajawahar14@gmail.com>
2026-09-10 16:29:03 +0530

    Create DuplicateChallanException.java

---
+++ TrafficViolationEChallan/src/main/java/com/traffic/exception/DuplicateChallanException.java
@@ -0,0 +1,8 @@
+package com.traffic.exception;
+
+public class DuplicateChallanException extends RuntimeException {
+
+    public DuplicateChallanException(String message) {
+        super(message);
+    }
+}
